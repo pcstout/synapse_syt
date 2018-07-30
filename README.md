a6e26e6020fa3707ceb6edfbfb2ee4c57827473d
Patrick <pcsout@gmail.com>
2018-07-30 14:46:03 -0600

    Added Synapse sync to check in/out methods. Other enhancements.

--- README.md
+++ README.md
@@ -1,25 +1,83 @@
 # Synapse Folder Check In/Out Tracking
 
-A utility to track [Synapse](https://www.synapse.org/) Folder check ins/outs.
+A utility to track [Synapse](https://www.synapse.org/) Project, Folder, and File check ins/outs.
 
 ## Usage
 
 ```
-usage: syt.py [-h] [-f] [-u USERNAME] [-p PASSWORD]
-              {checkout,checkin,show} entity-id
+usage: syt.py [-h] [-s] [-f] [-u USERNAME] [-p PASSWORD]
+              {checkout,checkin,show} entity-id [checkout-path]
 
 positional arguments:
   {checkout,checkin,show}
                         The command to execute.
-  entity-id             The ID of the Synapse Folder to execute the command
-                        on. Can be a Project ID for the show command.
+  entity-id             The ID of the Synapse Entity to execute the command
+                        on.
+  checkout-path         The local path to sync with Synapse.
 
 optional arguments:
   -h, --help            show this help message and exit
-  -f, --force           Force a check-in.
+  -s, --skip-sync       Do not download or upload when checking in/out.
+  -f, --force           Force a check in/out.
   -u USERNAME, --username USERNAME
                         Synapse username.
   -p PASSWORD, --password PASSWORD
                         Synapse password.
-
 ```
+
+## Examples
+
+Checkout a folder (`syn001`) into `my_study_data`:
+```bash
+$ syt.py checkout syn001 my_study_data
+Checking out...
+Logging into Synapse...
+Loading Entity...
+Loading Project...
+Folder "Folder B" (syn001) from Project "Test Project CheckInOut" (syn002)
+making dir my_study_data/Folder B-1
+Downloading  [####################]100.00%   17.0bytes/17.0bytes (53.2kB/s) test_file_A.txt Done...
+Downloading  [####################]100.00%   17.0bytes/17.0bytes (53.0kB/s) test_file_B.txt Done...
+Checked out files:
+  - ./my_study_data/Folder B-1/test_file_A.txt
+  - ./my_study_data/test_file_B.txt
+Check-out was successful
+```
+Note: After initial check-out you can re-check-out with `syt.py checkout my_study_data` or from inside `my_study_data` `syt.py checkout`. 
+
+Show the check-out status for `my_study_data`:
+```bash
+$ syt.py show my_study_data
+Show Checked out entities...
+Logging into Synapse...
+Loading Entity...
+Loading Project...
+Folder "Folder B" (syn001) from Project "Test  Project CheckInOut" (syn002)
+--------------------------------------------------------------------------------
+Entity: Folder B (syn001)
+Checked out by: my-user-name (3376664)
+Checked out on: 2018-07-30 11:32:41.884000
+```
+Note: Can also be done with `syt.py show syn001` or from inside `my_study_data` `syt.py show`.
+
+Check-in `my_study_data`:
+```bash
+$ syt.py checkin my_study_data
+Checking in...
+Logging into Synapse...
+Loading Entity...
+Loading Project...
+Folder "Folder B" (syn001) from Project "Test  Project CheckInOut" (syn002)
+Validation and upload of: my_study_data/SYNAPSE_METADATA_MANIFEST.tsv
+Validating columns of manifest.....OK
+Validating that all paths exist..OK
+Validating that all files are unique...OK
+Validating provenance...OK
+Validating that parents exist and are containers...OK
+==================================================
+We are about to upload 2 files with a total size of 34.0bytes.
+ ==================================================
+Starting upload...
+Check-in was successful
+```
+Note: Can also be done with `syt.py checkin syn001 my_study_data` or from inside `my_study_data` `syt.py checkin`.
--- syt.py
+++ syt.py
@@ -25,6 +25,7 @@ import synapseclient
 from synapseclient import Project, Folder, File, Team, TeamMember
 from synapseclient import Team, TeamMember
 from synapseclient import Schema, Column, Table, Row, RowSet
+import synapseutils
 
 
 class Syt:
@@ -33,6 +34,8 @@ class Syt:
     ANNO_CHECKED_OUT_BY_NAME = '_syt_by_name'
     ANNO_CHECKED_OUT_DATE = '_syt_date'
     ANNO_CHECKED_OUT_ETAG = '_syt_etag'
+    ALL_ANNO = [ANNO_CHECKED_OUT_BY_ID, ANNO_CHECKED_OUT_BY_NAME,
+                ANNO_CHECKED_OUT_DATE, ANNO_CHECKED_OUT_ETAG]
 
     def __init__(self, entity_id, username=None, password=None):
         self._synapse_client = None
@@ -51,36 +54,31 @@ class Syt:
 
     def show(self):
         print('Show Checked out entities...')
-        self.synapse_login()
+        if (not self._load()):
+            return
 
-        entity = self._synapse_client.get(self._entity_id, downloadFile=False)
-        print('{0} ({1})'.format(entity.name, entity.id))
+        entities = [self._entity]
 
-        folders = []
-
-        if (isinstance(entity, Folder)):
-            folders.append(entity)
-
-        folders += self._get_children(entity, Folder)
+        entities += self._get_children(self._entity)
 
         checked_out = []
 
-        for folder in folders:
-            if (self._is_checked_out(folder)):
-                checked_out.append(folder)
+        for entity in entities:
+            if (self._is_checked_out(entity)):
+                checked_out.append(entity)
 
         if (len(checked_out)):
-            for folder in checked_out:
+            for entity in checked_out:
                 print('-' * 80)
-                print('Entity: {0} ({1})'.format(folder.name, folder.id))
+                print('Entity: {0} ({1})'.format(entity.name, entity.id))
                 print('Checked out by: {0} ({1})'.format(
-                    folder[self.ANNO_CHECKED_OUT_BY_NAME][0], folder[self.ANNO_CHECKED_OUT_BY_ID][0]))
+                    entity[self.ANNO_CHECKED_OUT_BY_NAME][0], entity[self.ANNO_CHECKED_OUT_BY_ID][0]))
                 print('Checked out on: {0}'.format(
-                    folder[self.ANNO_CHECKED_OUT_DATE][0]))
+                    entity[self.ANNO_CHECKED_OUT_DATE][0]))
         else:
             print('No checked out entities found.')
 
-    def checkout(self):
+    def checkout(self, checkout_path=os.getcwd(), skip_sync=False, force=False):
         """
         Checks out an Entity.
         """
@@ -89,29 +87,53 @@ class Syt:
             return
 
         if (self._is_checked_out(self._entity)):
-            print('Entity already checked out. Aborting.')
-            return
+            if (force):
+                print('WARNING: Entity already checked out.')
+            else:
+                print('Entity already checked out. Aborting.')
+                return
 
         checked_out_parent = self._any_parents_checked_out()
         if (checked_out_parent != None):
-            print('Parent: {0} ({1}) is checked out. Aborting.'.format(
-                checked_out_parent.name, checked_out_parent.id))
-            return
+            if (force):
+                print('WARNING: Parent: {0} ({1}) is checked out.'.format(
+                    checked_out_parent.name, checked_out_parent.id))
+            else:
+                print('Parent: {0} ({1}) is checked out. Aborting.'.format(
+                    checked_out_parent.name, checked_out_parent.id))
+                return
 
         checked_out_child = self._any_children_checked_out()
         if (checked_out_child != None):
-            print('Child: {0} ({1}) is checked out. Aborting.'.format(
-                checked_out_child.name, checked_out_child.id))
-            return
+            if (force):
+                print('WANRING: Child: {0} ({1}) is checked out.'.format(
+                    checked_out_child.name, checked_out_child.id))
+            else:
+                print('Child: {0} ({1}) is checked out. Aborting.'.format(
+                    checked_out_child.name, checked_out_child.id))
+                return
+
+        if (not skip_sync):
+            # Download all the folders and files.
+            entities = synapseutils.syncFromSynapse(
+                self._synapse_client, self._entity, path=checkout_path)
+            print('')
+            print('Checked out files:')
+            for f in entities:
+                print('  - {0}'.format(f.path))
+
+            # Write the Synapse ID of the checked out object so we can easily get it for check-in.
+            Syt.write_dot_syt(checkout_path, self._entity.id)
 
         self._entity[self.ANNO_CHECKED_OUT_BY_ID] = self._user.ownerId
         self._entity[self.ANNO_CHECKED_OUT_BY_NAME] = self._user.userName
         self._entity[self.ANNO_CHECKED_OUT_DATE] = datetime.datetime.now()
         self._entity[self.ANNO_CHECKED_OUT_ETAG] = self._entity.etag
         self._synapse_client.store(self._entity)
+
         print('Check-out was successful')
 
-    def checkin(self, force=False):
+    def checkin(self, checkout_path=os.getcwd(), skip_sync=False, force=False):
         """
         Checks in an Entity.
         """
@@ -120,74 +142,95 @@ class Syt:
             return
 
         if (not self._is_checked_out(self._entity)):
-            print('Entity not checked out. Aborting.')
-            return
+            if (force):
+                print('WARNING: Entity not checked out.')
+            else:
+                print('Entity not checked out. Aborting.')
+                return
 
         if (not force and self._entity[self.ANNO_CHECKED_OUT_BY_ID][0] != self._user.ownerId):
-            print(
-                'Entity can only be checked in by the user that checked it out. Aborting.')
-            return
+            if (force):
+                print('WARNING: Entity is currently checked out by another user.')
+            else:
+                print(
+                    'Entity can only be checked in by the user that checked it out. Aborting.')
+                return
 
-        for key in [self.ANNO_CHECKED_OUT_BY_ID, self.ANNO_CHECKED_OUT_BY_NAME, self.ANNO_CHECKED_OUT_DATE, self.ANNO_CHECKED_OUT_ETAG]:
+        # Upload the files
+        if (not skip_sync):
+            manifest_filename = os.path.join(
+                checkout_path, 'SYNAPSE_METADATA_MANIFEST.tsv')
+            if (os.path.exists(manifest_filename)):
+                synapseutils.syncToSynapse(
+                    self._synapse_client, manifest_filename, sendMessages=False)
+            else:
+                print(
+                    'Manifest file not found in: "{0}". Folder/Files will not be uploaded to Synapse.'.format(checkout_path))
+
+        for key in self.ALL_ANNO:
             if key in self._entity.annotations:
                 del self._entity.annotations[key]
 
         self._synapse_client.store(self._entity)
+
         print('Check-in was successful')
 
     def _is_checked_out(self, entity):
         return self.ANNO_CHECKED_OUT_BY_ID in entity
 
     def _any_parents_checked_out(self):
-        parents = self._get_parents(self._entity, parent_type=Folder)
+        """
+        Gets if any parent entities are checked out.
+        """
+        parents = self._get_parents(self._entity, [Project, Folder])
         for folder in parents:
             if (self._is_checked_out(folder)):
                 return folder
 
     def _any_children_checked_out(self):
-        children = self._get_children(self._entity, child_type=Folder)
+        """
+        Gets if any child entities are checked out.
+        """
+        children = self._get_children(self._entity)
         for folder in children:
             if (self._is_checked_out(folder)):
                 return folder
 
-    def _get_parents(self, child_entity, parent_type=Project):
+    def _get_parents(self, child_entity, parent_types=[Project]):
         """
-        Gets all the parent entities of a specific type
+        Gets all the parent entities of a specific type.
         """
+        results = []
+
         parent_id = child_entity.parentId
 
-        if (parent_id == None):
-            return None
-
-        results = []
+        if (parent_id == None or isinstance(child_entity, Project)):
+            return results
 
         parent = self._synapse_client.get(parent_id)
 
-        if (isinstance(parent, parent_type)):
-            results.append(parent)
+        for parent_type in parent_types:
+            if (isinstance(parent, parent_type)):
+                results.append(parent)
 
-        # Do not go past the Project level.
-        if (parent.parentId and not isinstance(parent, Project)):
-            results += self._get_parents(parent, parent_type)
+        results += self._get_parents(parent, parent_types)
 
         return results
 
-    def _get_children(self, parent_entity, child_type=Folder):
+    def _get_children(self, parent_entity):
         """
-        Get all the child entities of a specific type
+        Get all the child Folders and Files.
         """
         results = []
 
-        child_type_str = child_type._synapse_entity_type.split('.')[-1].lower()
-
         children = self._synapse_client.getChildren(
-            parent_entity, includeTypes=[child_type_str])
+            parent_entity, includeTypes=['folder', 'file'])
 
         for child in children:
             child_entity = self._synapse_client.get(
                 child['id'], downloadFile=False)
             results.append(child_entity)
-            results += self._get_children(child_entity, child_type)
+            results += self._get_children(child_entity)
 
         return results
 
@@ -201,15 +244,16 @@ class Syt:
 
         type = self._entity.entityType.split('.')[-1].replace('Entity', '')
 
-        if type in ['Folder']:
-            print('Loading Project...')
-            self._project = self._get_parents(self._entity, Project)[0]
-            print('{0} {1} ({2}) from Project {3} ({4})'.format(
-                type, self._entity.name, self._entity.id, self._project.name, self._project.id))
+        if type in ['Project', 'Folder', 'File']:
+            if (type != 'Project'):
+                print('Loading Project...')
+                self._project = self._get_parents(self._entity, [Project])[0]
+                print('{0} "{1}" ({2}) from Project "{3}" ({4})'.format(
+                    type, self._entity.name, self._entity.id, self._project.name, self._project.id))
         else:
             print('Found {0} {1} ({2})'.format(
                 type, self._entity.name, self._entity.id))
-            print('Only Folders can be checked in/out. Aborting.')
+            print('Only Projects, Folders, and Files can be checked in/out. Aborting.')
             self._entity = None
             self._project = None
 
@@ -232,27 +276,76 @@ class Syt:
         self._synapse_client = synapseclient.Synapse()
         self._synapse_client.login(syn_user, syn_pass, silent=True)
 
+    @staticmethod
+    def get_dot_syt_filename(path):
+        """
+        Gets the full filename of the .syt file.
+        """
+        return os.path.join(path, '.syt')
+
+    @staticmethod
+    def write_dot_syt(path, entity_id):
+        """
+        Writes to the .syt file.
+        """
+        with open(Syt.get_dot_syt_filename(path), "w") as syt_file:
+            syt_file.write(entity_id)
+
+    @staticmethod
+    def read_dot_syt(path):
+        """
+        Reads the .syt file.
+        """
+        if (not os.path.exists(Syt.get_dot_syt_filename(path))):
+            return None
+
+        with open(Syt.get_dot_syt_filename(path), "r") as syt_file:
+            return syt_file.read()
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('command', choices=[
                         'checkout', 'checkin', 'show'], help='The command to execute.')
-    parser.add_argument('entity_id', metavar='entity-id',
-                        help='The ID of the Synapse Folder to execute the command on. Can be a Project ID for the show command.')
-    parser.add_argument(
-        '-f', '--force', help='Force a check-in.', default=False, action='store_true')
+    parser.add_argument('entity_id', metavar='entity-id', nargs='?',
+                        help='The ID of the Synapse Entity to execute the command on.')
+    parser.add_argument('checkout_path', metavar='checkout-path',
+                        nargs='?', default=os.getcwd(), help='The local path to sync with Synapse.')
+    parser.add_argument('-s', '--skip-sync', help='Do not download or upload when checking in/out.',
+                        default=False, action='store_true')
+    parser.add_argument('-f', '--force', help='Force a check in/out.',
+                        default=False, action='store_true')
     parser.add_argument('-u', '--username',
                         help='Synapse username.', default=None)
     parser.add_argument('-p', '--password',
                         help='Synapse password.', default=None)
     args = parser.parse_args()
 
-    syt = Syt(args.entity_id, username=args.username, password=args.password)
+    entity_id = args.entity_id
+    checkout_path = args.checkout_path
+
+    # Load the entity_id from .syt if not specified.
+    if (not entity_id):
+        checkout_path = os.getcwd()
+        entity_id = Syt.read_dot_syt(checkout_path)
+        if (not entity_id):
+            print('Entity ID not specified, .syt file not found in: {0}'.format(
+                Syt.get_dot_syt_filename(checkout_path)))
+            return
+    elif (not entity_id.lower().startswith('syn')):
+        checkout_path = entity_id
+        entity_id = Syt.read_dot_syt(entity_id)
+        if (not entity_id):
+            print(
+                'Entity ID not specified, .syt file not found in checkout-path: {0}'.format(checkout_path))
+            return
+
+    syt = Syt(entity_id, username=args.username, password=args.password)
 
     if args.command == 'checkin':
-        syt.checkin(force=args.force)
+        syt.checkin(checkout_path, args.skip_sync, args.force)
     elif args.command == 'checkout':
-        syt.checkout()
+        syt.checkout(checkout_path, args.skip_sync, args.force)
     else:
         syt.show()
 
